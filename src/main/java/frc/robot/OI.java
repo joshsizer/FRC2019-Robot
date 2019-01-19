7f675f3f4a57914b30879cc4f1d2b845900bcaf1
Josh Sizer <joshsizer@gmail.com>
2019-01-19 13:45:11 -0500

    Added operator input

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,17 +1,47 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by FRC teams. The code */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
+/* the project. */
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import frc.robot.commands.PopPopper;
+import frc.robot.utilities.XboxController;
+
 /**
- * This class is the glue that binds the controls on the physical operator
- * interface to the commands and command groups that allow control of the robot.
+ * This class is the glue that binds the controls on the physical operator interface to the commands
+ * and command groups that allow control of the robot.
  */
 public class OI {
+  public static XboxController mDriverController;
+
+  public OI() {
+    mDriverController = new XboxController(RobotMap.kDriverControllerPort);
+
+    // set our commands for each button
+    // mDriverController.ButtonA.whileHeld(new PopPopper());
+  }
+
+  public void run() {
+    Robot.Drive.setSpeedTurn(mDriverController.getY(Hand.kLeft),
+        mDriverController.getX(Hand.kRight));
+
+    mDriverController.ButtonA.whileHeld(new PopPopper());
+
+
+    // if (mDriverController.getAButton()) {
+    // Robot.Popper.pop();
+    // } else {
+    // Robot.Popper.retract();
+    // }
+  }
+
+
+
   //// CREATING BUTTONS
   // One type of button is a joystick button which is any button on a
   //// joystick.
